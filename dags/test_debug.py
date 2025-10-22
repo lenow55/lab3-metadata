96bb0a4876d793bfba99de25160c5c1ee982c1dc
INovikov Home <nov.ilucha@yandex.ru>
2025-10-22 09:29:18 +0300

    now debugging works

--- dags/test_debug.py
+++ dags/test_debug.py
@@ -5,14 +5,24 @@ from typing import Any
 from airflow.sdk import dag, task  # pyright: ignore[reportUnknownVariableType]
 
 
-@task(task_id="print_the_context")
+@task(task_id="task_1")
 def debug_task(ds: Any = None, **kwargs) -> str:
-    print("Start dag")
+    print("Start task")
     x = 2 + 2
     print(f"x={x}")
     print(ds)
     pprint(kwargs)
     return "Whatever you return gets printed in the logs"
+
+
+@task(task_id="task_2")
+def debug_task2(ds: Any = None, **kwargs) -> str:
+    print("Start task2")
+    x = 2 + 2
+    print(f"x={x}")
+    print(ds)
+    pprint(kwargs)
+    return "Whatever you return gets printed in the logs 2"
 
 
 @dag(
@@ -24,8 +34,9 @@ def debug_task(ds: Any = None, **kwargs) -> str:
 )
 def example_debug_dag():
     run_this = debug_task()
+    run_2_this = debug_task2()
 
-    run_this
+    run_this >> run_2_this
 
 
 debug_dag = example_debug_dag()

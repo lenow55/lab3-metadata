97c2a3c4675de075b9fa7ef9060ee5fd36ef5aaf
INovikov Home <nov.ilucha@yandex.ru>
2025-10-26 12:15:57 +0300

    add tables for lab

--- postgres-init/init_db.sql
+++ postgres-init/init_db.sql
@@ -2,19 +2,40 @@ CREATE DATABASE backend;
 
 \connect backend;
 
-CREATE TABLE IF NOT EXISTS public.order_events (
+CREATE TABLE IF NOT EXISTS public.source (
             id SERIAL PRIMARY KEY,
-            order_id INTEGER NOT NULL,
-            status VARCHAR(50) NOT NULL,
-            ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            column1 VARCHAR(50),
+            i_column2 INTEGER,
+            column3 VARCHAR(50),
+            i_column4 INTEGER,
+            column5 VARCHAR(50),
+            i_column6 INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS public.target (
+            id SERIAL PRIMARY KEY,
+            ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            column7 VARCHAR(50),
+            i_column8 INTEGER,
+            column9 VARCHAR(50),
+            i_column10 INTEGER,
+            column11 VARCHAR(50),
+            i_column12 INTEGER,
 );
 
 CREATE DATABASE metadata;
 
 \connect metadata;
 
-CREATE TABLE public.s3_max_dates (
+CREATE TABLE public.transfer_max_dates (
     table_name TEXT PRIMARY KEY,
     max_date DATE,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+);
+
+CREATE TABLE public.transfer_metadata (
+    id SERIAL PRIMARY KEY,
+    source_name TEXT,
+    target_name TEXT,
 );

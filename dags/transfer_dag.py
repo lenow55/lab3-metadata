7f2a9f799ebdc6ef8a4f47f88e583ca85cef84d0
Новиков Илья <nov.ilucha@yandex.ru>
2025-10-29 19:13:00 +0300

    reorganize for make transfer manager as plugin

--- dags/transfer_dag.py
+++ dags/transfer_dag.py
@@ -1,8 +1,21 @@
 import logging
 
-from airflow.sdk import dag, task  # pyright: ignore[reportUnknownVariableType]
+from airflow.exceptions import AirflowNotFoundException
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.sdk import task
 
-from .schemas import metadata_backend, metadata_md
-from .transfer_manager import TransferDateTimeManager
+from dags.schemas import metadata_table
 
 logger = logging.getLogger(__name__)
+
+
+@task(task_id="load_transfer_maping")
+def load_transfer_maping():
+    logger.info("Start task get transfer columns")
+    hook = PostgresHook(postgres_conn_id="metadata_conn_id")
+    engine = hook.get_sqlalchemy_engine()
+    smth = metadata_table.select().compile(bind=engine)
+    map_df = hook.get_pandas_df(smth.string)
+    if map_df.empty:
+        logger.warning("Empty Transfer Table")
+        raise AirflowNotFoundException("No columns to transfer")
